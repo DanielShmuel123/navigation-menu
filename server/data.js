ed25b23b619e1620caf8757a3daaf2f6d3a6686a
DanielShmuel123 <45069092+DanielShmuel123@users.noreply.github.com>
2022-10-25 10:37:17 +0200

    initial commit for server project

---
+++ server/data.js
@@ -0,0 +1,74 @@
+export const projectData = [
+  {
+    id: 736,
+    name: "Systems",
+    image: {
+      link: "http://i.stack.imgur.com/8KA9j.jpg?s=32&g=1",
+    },
+    groups: [
+      {
+        id: 2168,
+        name: "API",
+
+        url: "https://www.itschools.co.za/api/",
+      },
+      {
+        id: 11955,
+        name: "Assets",
+        url: "https://www.itschools.co.za/assets/",
+      },
+      {
+        id: 3179,
+        name: "Design",
+        url: "https://www.itschools.co.za/design/",
+      },
+      {
+        id: 207,
+        name: "Development",
+        url: "https://www.itschools.co.za/development/",
+      },
+      {
+        id: 70,
+        name: "Intranet",
+        url: "https://www.itschools.co.za/intranet/",
+      },
+    ],
+    url: "https://www.itschools.co.za/projects",
+  },
+  {
+    id: 44315,
+    name: "User Agents",
+    image: {
+      link: " https://cdn.pixabay.com/photo/2018/09/24/08/31/pixel-cells-3699334_960_720.png",
+    },
+    groups: [
+      {
+        id: 191599,
+
+        name: "Alchemy",
+        url: "https://www.itschools.co.za/tools/alchemy",
+      },
+      {
+        id: 86822,
+        name: "Empathy",
+        url: "https://www.itschools.co.za/tools/empathy",
+      },
+      {
+        id: 86297,
+        name: "Epiphany",
+        url: "https://www.itschools.co.za/tools/epiphany",
+      },
+      {
+        id: 131837,
+        name: "Harmony",
+        url: "https://www.itschools.co.za/tools/hamony",
+      },
+      {
+        id: 174338,
+        name: "Zagreb",
+        url: "https://www.itschools.co.za/tools/zagreb",
+      },
+    ],
+    url: "https://www.itschools.co.za/tools",
+  },
+];
---
+++ server/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import dotenv from "dotenv";
+import { projectData } from "./data.js";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  })
+);
+const port = process.env.PORT;
+
+app.get("/projects", (req, res) => {
+  res.json(projectData);
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at https://localhost:${port}`);
+});
